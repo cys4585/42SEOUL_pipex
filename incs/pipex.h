0ceb8d845e506a6dee883b34352d6cdc3c068976
youngcho <soc4585@naver.com>
2022-08-18 16:40:35 +0900

    불필요 소스파일 삭제

--- incs/pipex.h
+++ incs/pipex.h
@@ -6,7 +6,7 @@
 /*   By: youngcho <youngcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 18:49:21 by youngcho          #+#    #+#             */
-/*   Updated: 2022/08/18 14:54:02 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/08/18 16:40:09 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,5 @@
 // exit
 #include <fcntl.h>
 // open
-
-typedef enum e_mode
-{
-	NORMAL,
-	HERE_DOC
-}	t_mode;
-
-typedef struct s_args
-{
-	t_mode	mode;
-	char	*infile;
-	char	*outfile;
-	char	**first_cmd_p;
-	char	**last_cmd_p;
-}	t_args;
-
-void	set_args(t_args *args_p, int argc, char *argv[]);
 
 #endif
