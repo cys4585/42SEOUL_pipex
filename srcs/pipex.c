9a1f6d5ca02faed565e2aa34a88a2af919144fb5
youngcho <soc4585@naver.com>
2022-08-20 11:40:51 +0900

    Mandatory 완료

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: youngcho <youngcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 18:44:13 by youngcho          #+#    #+#             */
-/*   Updated: 2022/08/20 11:34:54 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/08/20 11:37:51 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ int	main(int argc, char *argv[], char *envp[])
 	check_custom_error(CUS_REQ_ARG, "pipex: Requires 4 arguments.\n", &argc);
 	redirect(argv[1], argv[argc -1], &fd_infile, &fd_outfile);
 	execute_first_cmd(argv[2], envp);
-	execute_last_cmd(argv[3], envp);
+	execute_last_cmd(argv[argc -2], envp);
 	return (0);
 }
